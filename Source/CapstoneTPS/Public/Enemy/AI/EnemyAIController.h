6a414aeb365cb2aaa05dcd438e7e95bf8b3d54d1
img98 <nichy98@naver.com>
2023-03-26 23:40:57 +0900

    feat : AI컨트롤러 BT, BB 제작 시작

---
+++ Source/CapstoneTPS/Public/Enemy/AI/EnemyAIController.h
@@ -0,0 +1,26 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "AIController.h"
+#include "EnemyAIController.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class CAPSTONETPS_API AEnemyAIController : public AAIController
+{
+	GENERATED_BODY()
+	
+public:
+	AEnemyAIController();
+
+protected:
+
+
+private:
+
+
+};
